e49bf5e728b15fced8ddaee4afa01debe5faa4e5
magic76 <shanliyoung@gmail.com>
2026-09-18 16:52:07 +0800

    Run RuntimeToolRouting boundary test

--- scripts/run_agent_runtime_tests.sh
+++ scripts/run_agent_runtime_tests.sh
@@ -26,6 +26,7 @@ javac -encoding UTF-8 -d "$OUT" \
   "$SRC/SendAuthorization.java" \
   "$SRC/LiveTurnCoordinator.java" \
   "$SRC/ToolCallDispatchLedger.java" \
+  "$SRC/RuntimeToolRouting.java" \
   "$SRC/UiChangeSignal.java" \
   "$SRC/RuntimeUiState.java" \
   "$SRC/LearnedUiScopePolicy.java" \
@@ -49,6 +50,7 @@ javac -encoding UTF-8 -d "$OUT" \
   "$ROOT/tests/SendAuthorizationTest.java" \
   "$ROOT/tests/LiveTurnCoordinatorTest.java" \
   "$ROOT/tests/ToolCallDispatchLedgerTest.java" \
+  "$ROOT/tests/RuntimeToolRoutingTest.java" \
   "$ROOT/tests/UiChangeSignalTest.java" \
   "$ROOT/tests/RuntimeUiStateTest.java" \
   "$ROOT/tests/LearnedUiScopePolicyTest.java" \
@@ -79,6 +81,7 @@ javac -encoding UTF-8 -d "$OUT" \
 java -cp "$OUT" com.crewpocket.helper.SendAuthorizationTest
 java -cp "$OUT" com.crewpocket.helper.LiveTurnCoordinatorTest
 java -cp "$OUT" com.crewpocket.helper.ToolCallDispatchLedgerTest
+java -cp "$OUT" com.crewpocket.helper.RuntimeToolRoutingTest
 java -cp "$OUT" com.crewpocket.helper.UiChangeSignalTest
 java -cp "$OUT" com.crewpocket.helper.RuntimeUiStateTest
 java -cp "$OUT" com.crewpocket.helper.LearnedUiScopePolicyTest
