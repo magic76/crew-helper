da8b735eaa78089da366d23e92c5cf046b221cc9
magic76 <shanliyoung@gmail.com>
2026-09-24 20:35:53 +0800

    Run implicit retry regression tests

--- scripts/run_agent_runtime_tests.sh
+++ scripts/run_agent_runtime_tests.sh
@@ -50,6 +50,7 @@ javac -encoding UTF-8 -d "$OUT" \
   "$SRC/ElementReferenceChoice.java" \
   "$SRC/ElementReferenceLayout.java" \
   "$SRC/GoalTaskContinuityPolicy.java" \
+  "$SRC/UserRetryAfterUnconfirmedOutcomePolicy.java" \
   "$SRC/InspectorTaskState.java" \
   "$SRC/InspectorTraceScope.java" \
   "$SRC/ReflectionLearningPolicy.java" \
@@ -110,6 +111,7 @@ javac -encoding UTF-8 -d "$OUT" \
   "$ROOT/tests/ElementReferenceChoiceTest.java" \
   "$ROOT/tests/ElementReferenceLayoutTest.java" \
   "$ROOT/tests/GoalTaskContinuityPolicyTest.java" \
+  "$ROOT/tests/UserRetryAfterUnconfirmedOutcomePolicyTest.java" \
   "$ROOT/tests/InspectorTaskStateTest.java" \
   "$ROOT/tests/InspectorTraceScopeTest.java" \
   "$ROOT/tests/ReflectionLearningPolicyTest.java" \
@@ -170,6 +172,7 @@ java -cp "$OUT" com.crewpocket.helper.ElementReferenceCommandTest
 java -cp "$OUT" com.crewpocket.helper.ElementReferenceChoiceTest
 java -cp "$OUT" com.crewpocket.helper.ElementReferenceLayoutTest
 java -cp "$OUT" com.crewpocket.helper.GoalTaskContinuityPolicyTest
+java -cp "$OUT" com.crewpocket.helper.UserRetryAfterUnconfirmedOutcomePolicyTest
 java -cp "$OUT" com.crewpocket.helper.InspectorTaskStateTest
 java -cp "$OUT" com.crewpocket.helper.InspectorTraceScopeTest
 java -cp "$OUT" com.crewpocket.helper.ReflectionLearningPolicyTest
